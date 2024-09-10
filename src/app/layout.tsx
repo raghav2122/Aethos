be1d4e250e5c8510addff63c3120002babb43745
raghav2122 <raghavsharma0257@gmail.com>
2024-09-11 00:02:11 +0530

    project setup

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { cn } from "@/lib/utils";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" h-full>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={cn('relative  antialiased')}>
+          <main className="relative flex flex-col min-h-screen">
+            <div className="flex-grow flex-1">
+            {children}
+            </div>
+          </main>
       </body>
     </html>
   );
